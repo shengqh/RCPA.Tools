b2d09bf1d1258674623b71d90ca61fb76f6af044
shengqh <shengqh@gmail.com>
2014-03-10 09:37:34 -0500

    sqh

---
+++ Properties/RcpaToolsAssembly.cs
@@ -0,0 +1,20 @@
+﻿#region License
+//
+// Weighted Somatic Mutation Detector: ThisAssembly.cs
+//
+// Author:
+//   Quanhu SHENG (quanhu.sheng@vanderbilt.edu/shengqh@gmail.com)
+//
+// Copyright (C) 2012 CQS/VUMC
+//
+#endregion
+
+static class RcpaToolsAssembly
+{
+  internal const string Title = "Proteomics Tool Suite";
+  internal const string Author = "Quanhu SHENG";
+  internal const string Company = "Research Center for Proteome Analysis, CAS, China";
+  internal const string Copyright = "Copyright (C) 2006-2013 " + Company;
+  internal const string Version = "3.4.9.0";
+  internal const string InformationalVersion = Version;
+}
